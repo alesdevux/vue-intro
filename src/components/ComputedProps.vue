0fb14e1c8dd05ec72d975707234881794302d7ab
Ales Web <aleswebgit@gmail.com>
2022-04-18 12:50:14 +0200

    computed properties

---
+++ src/components/ComputedProps.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      dateNow: new Date()
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`
+    },
+    date() {
+      return this.dateNow.toLocaleString()
+    }
+  }
+}
+
+</script>
+
+<template>
+<div class="f-small">
+  <p>{{ fullName }}</p>
+  <p>{{ date }}</p>
+</div>
+</template>
