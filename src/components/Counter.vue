14e9fd7cf1b8e028a1a3603d787e086be4676389
Ales Web <aleswebgit@gmail.com>
2022-04-18 02:35:09 +0200

    counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const counter = ref(0)
+
+function decrement() {
+  if (counter.value > 0) {
+    counter.value--
+  }
+}
+function increment() {
+  counter.value++
+}
+</script>
+
+<template>
+  <h1 class="mb-32">{{ msg }}</h1>
+
+  <button @click="decrement">-</button>
+  <span class="px-5">{{ counter }}</span>
+  <button @click="increment">+</button>
+</template>
